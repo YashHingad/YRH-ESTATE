dce2dccf1157128c00fb59696eb71c686c343a1b
Yash Jain <yrh1263@gmail.com>
2024-08-11 19:05:38 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import PrivateRoute from './Components/PrivateRoute';
+
+import About from "./pages/About";
+import CreateListing from "./pages/CreateListing";
+import Home from "./pages/Home";
+import Listing from "./pages/Listing";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import UpdateListing from "./pages/UpdateListing";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+        <Route path = "/search" element = {<Search/>} />
+        <Route path="/listing/:listingId" element={<Listing />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-listing" element={<CreateListing />} />
+          <Route
+            path="/update-listing/:listingId"
+            element={<UpdateListing />}
+          />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
