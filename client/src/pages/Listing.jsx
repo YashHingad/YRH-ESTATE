313d99ef56d902badef2f06dd8dfc430526d5b8d
Yash Hingad <114935100+YashHingad@users.noreply.github.com>
2025-10-18 20:00:58 +0530

    Optimized image loading for property thumbnail

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -141,7 +141,7 @@ export default function Listing() {
                 onClick={() => setContact(true)}
                 className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'
               >
-                Contact landlord
+                Contact Landlord
               </button>
             )}
             {contact && <Contact listing={listing} />}
