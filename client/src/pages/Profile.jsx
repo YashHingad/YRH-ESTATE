8c264a3072cb2179d13850d3d23b595910b63b09
Yash Hingad <114935100+YashHingad@users.noreply.github.com>
2025-10-18 19:02:08 +0530

    Updated Profile Page and fixed profile photo display issue

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -250,7 +250,7 @@ export default function Profile() {
         <div className=" flex flex-col gap-4">
           <h1 className="text-center mt-7 text-2xl font-semibold ">
             {" "}
-            Your Listing
+            Your Listings
           </h1>
           {userListings.map((listing) => (
             <div
