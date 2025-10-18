469167f6892356b472f14cddc2cba5aa1c88b1fc
Yash Hingad <114935100+YashHingad@users.noreply.github.com>
2025-10-18 19:07:38 +0530

    Improved error message Listing function

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -32,7 +32,7 @@ export const deleteListing = async (req, res, next) => {
 export const updateListing = async (req, res, next) => {
   const listing = await Listing.findById(req.params.id);
   if(!listing){
-    return next(errorHandler(404, 'Listing not found'));
+    return next(errorHandler(404, 'Listing not found!'));
   }
   if(req.user.id !== listing.userRef){
     return next(errorHandler(401, 'You can update your own listing'));
